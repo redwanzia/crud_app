7e832367e12e99213012c1894aff8c836eae7722
redwan zia <redwan.zia@gmail.com>
2020-12-26 18:42:12 -0500

    added structure and submit functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [ inputItem, setInputItem ] = useState('');
+	const [ list, setList ] = useState([]);
+	const [ isEditing, setIsEditing ] = useState(false);
+	const [ editId, setEditId ] = useState(null);
+	const [ alert, setAlert ] = useState({ show: false, msg: '', type: '' });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!inputItem) {
+			// handle alert
+		} else if (inputItem && isEditing) {
+			// handle edit
+		} else {
+			const newListItem = { id: new Date().getTime().toString(), title: inputItem };
+			setList([ ...list, newListItem ]);
+			setInputItem('');
+		}
+	};
+
+	return (
+		<section className='section-center' onClick={handleSubmit}>
+			<form className='grocery-form'>
+				{alert.show && <Alert />}
+				<h3>CRUD list</h3>
+				<div className='form-control'>
+					<input className='' type='text' value={inputItem} onChange={(e) => setInputItem(e.target.value)} />
+					<button type='submit' className='submit-btn'>
+						{isEditing ? 'edit' : 'submit'}
+					</button>
+				</div>
+			</form>
+			{list.length > 0 && (
+				<div className='grocery-container'>
+					<List lists={list} />
+					<button className='clear-btn'>clear item</button>
+				</div>
+			)}
+		</section>
+	);
 }
 
-export default App
+export default App;
--- src/List.js
+++ src/List.js
@@ -1,7 +1,26 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+const List = ({ lists }) => {
+	return (
+		<div className='grocery-list'>
+			{lists.map((list) => {
+				const { ud, title } = list;
+				return (
+					<article key className='grocery-item'>
+						<p className='title'>{title}</p>
+						<div className='btn-container'>
+							<button type='button' className='edit-btn'>
+								<FaEdit />
+							</button>
+							<button type='button' className='delete-btn'>
+								<FaTrash />
+							</button>
+						</div>
+					</article>
+				);
+			})}
+		</div>
+	);
+};
 
-export default List
+export default List;
