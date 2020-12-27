e9dfa9b2ce7839182def1fa9b051dcda2e60b004
redwan zia <redwan.zia@gmail.com>
2020-12-27 11:39:44 -0500

    added add remove and edit fucntionalities

--- src/App.js
+++ src/App.js
@@ -2,33 +2,87 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+	let list = localStorage.getItem('list');
+	if (list) {
+		return JSON.parse(list);
+	} else {
+		return [];
+	}
+};
+
 function App() {
 	const [ inputItem, setInputItem ] = useState('');
-	const [ list, setList ] = useState([]);
+	const [ list, setList ] = useState(getLocalStorage());
 	const [ isEditing, setIsEditing ] = useState(false);
-	const [ editId, setEditId ] = useState(null);
-	const [ alert, setAlert ] = useState({ show: false, msg: '', type: '' });
+	const [ editID, setEditID ] = useState(null);
+	const [ alert, setAlert ] = useState({ show: false, type: '', msg: '' });
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (!inputItem) {
-			// handle alert
+			showAlert(true, 'danger', 'Please enter value');
 		} else if (inputItem && isEditing) {
-			// handle edit
+			setList(
+				list.map((item) => {
+					if (item.id === editID) {
+						return { ...item, title: inputItem };
+					}
+					return item;
+				})
+			);
+			setInputItem('');
+			setEditID(null);
+			setIsEditing(false);
+			showAlert(true, 'success', 'value changed');
 		} else {
+			showAlert(true, 'success', 'item added to the list');
 			const newListItem = { id: new Date().getTime().toString(), title: inputItem };
 			setList([ ...list, newListItem ]);
 			setInputItem('');
 		}
 	};
 
+	const showAlert = (show = false, type = '', msg = '') => {
+		setAlert({ show, type, msg });
+	};
+	const clearList = () => {
+		showAlert(true, 'danger', 'empty list');
+		setList([]);
+	};
+
+	const removeItem = (id) => {
+		showAlert(true, 'danger', 'item remove');
+		setList(list.filter((item) => item.id !== id));
+	};
+
+	const editItem = (id) => {
+		const specificItems = list.find((item) => item.id === id);
+		setIsEditing(true);
+		setEditID(id);
+		setInputItem(specificItems.title);
+	};
+
+	useEffect(
+		() => {
+			localStorage.setItem('list', JSON.stringify(list));
+		},
+		[ list ]
+	);
+
 	return (
-		<section className='section-center' onClick={handleSubmit}>
-			<form className='grocery-form'>
-				{alert.show && <Alert />}
+		<section className='section-center'>
+			<form className='grocery-form' onSubmit={handleSubmit}>
+				{alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
 				<h3>CRUD list</h3>
 				<div className='form-control'>
-					<input className='' type='text' value={inputItem} onChange={(e) => setInputItem(e.target.value)} />
+					<input
+						className='grocery'
+						type='text'
+						placeholder='e.g. eggs'
+						value={inputItem}
+						onChange={(e) => setInputItem(e.target.value)}
+					/>
 					<button type='submit' className='submit-btn'>
 						{isEditing ? 'edit' : 'submit'}
 					</button>
@@ -36,8 +90,10 @@ function App() {
 			</form>
 			{list.length > 0 && (
 				<div className='grocery-container'>
-					<List lists={list} />
-					<button className='clear-btn'>clear item</button>
+					<List lists={list} removeItem={removeItem} editItem={editItem} />
+					<button className='clear-btn' onClick={clearList}>
+						clear item
+					</button>
 				</div>
 			)}
 		</section>
--- src/List.js
+++ src/List.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-const List = ({ lists }) => {
+const List = ({ lists, removeItem, editItem }) => {
 	return (
 		<div className='grocery-list'>
 			{lists.map((list) => {
-				const { ud, title } = list;
+				const { id, title } = list;
 				return (
-					<article key className='grocery-item'>
+					<article key={id} className='grocery-item'>
 						<p className='title'>{title}</p>
 						<div className='btn-container'>
-							<button type='button' className='edit-btn'>
+							<button type='button' className='edit-btn' onClick={() => editItem(id)}>
 								<FaEdit />
 							</button>
-							<button type='button' className='delete-btn'>
+							<button type='button' className='delete-btn' onClick={() => removeItem(id)}>
 								<FaTrash />
 							</button>
 						</div>
